1b9943a6dedf12ec18a0b7e7a91b31c062bbb24e
Omri Cohen <omricohen101@gmail.com>
2021-05-24 18:43:01 +0300

    minor adding + add ValueOutOfRangeException.cs

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -9,22 +9,52 @@ namespace Ex03.GarageLogic
     {
         private String m_ModelName;
         private String m_LicenseNumber;
-        private List<Wheel> m_Wheels;
+        private readonly List<Wheel> r_Wheels;
         private EnergySource m_Engine;
+        private readonly eNumberOfWheel r_NumberOfWheels;
 
-        private List<Wheel> Wheels
+        protected Vehicle()
         {
-            get { return m_Wheels; }
+            m_ModelName = String.Empty;
+            m_LicenseNumber = String.Empty;
+            r_Wheels = new List<Wheel>();
         }
-        protected void inflateTiresToMaximum()
+
+        internal enum eNumberOfWheel
+        {
+            Two = 2,
+            Four = 4,
+            Sixteen = 16 
+        }
+
+        internal eNumberOfWheel NumberOfWheel
+        {
+            get { return NumberOfWheel;  }
+        }
+        internal String ModelName
+        {
+            get { return m_ModelName; }
+            set { m_ModelName = value; }
+        }
+        internal String LicenseNumber
+        {
+            get { return m_LicenseNumber; }
+            set { m_LicenseNumber = value; }
+        }
+        internal List<Wheel> Wheels
+        {
+            get { return r_Wheels; }
+        }
+
+        
+        
+
+        protected void InflateTiresToMaximum()
         {
             foreach(Wheel wheel in Wheels)
             {
-                wheel.inflateToMaximum();
+                wheel.InflateToMaximum();
             }
         }
-     
     }
-
-   
 }
