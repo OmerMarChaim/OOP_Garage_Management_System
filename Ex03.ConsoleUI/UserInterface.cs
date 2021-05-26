9c6592bf93e275c4e145244836736cbf67fd4c31
Omer MarChaim <omermar7@gmail.com>
2021-05-26 13:36:51 +0300

    start UI

---
+++ Ex03.ConsoleUI/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex03.ConsoleUI
+{
+    class UserInterface
+    {
+        internal void startMenu()
+        {
+
+            string helloMessage = string.Format("Welcome to Our garage");
+            Console.WriteLine(helloMessage);
+            
+            string MenuOption = @"which service do you need ? please write the number :
+1. “Insert” a new vehicle into the garage.
+2. Display a list of license numbers currently in the garage
+3. Change a certain vehicle’s status
+4. Inflate your car tires to maximum
+5. Refuel a fuel-based vehicle
+6. Charge an electric-based vehicle
+7. Display specific vehicle information
+";
+            Console.WriteLine(MenuOption);
+        }
+    }
+}
